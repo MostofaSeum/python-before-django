2e8e6ada9e7444cf353aff45225b4d778c7237ad
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-18 16:47:41 +0600

    feat: add grade checker script with conditional grading logic

--- day-02/grade_checker.py
+++ day-02/grade_checker.py
@@ -1,4 +1,21 @@
 # Video Link: https://youtu.be/lIId8IDP6TU?si=fSYHm6h4j-zIPIw9
 
-a = input("Enter your name")
-print(len(a))
+a = int(input("Enter your marks: "))
+
+
+if a>=0 and a<=100:
+    if a >= 80:
+        print("Grade: A+")
+    elif a >= 70:
+        print("Grade: A")
+    elif a >= 60:
+        print("Grade: A-")
+    elif a >= 50:
+        print("Grade: B")
+    elif a > 40:
+        print("Grade: C")
+    elif a >= 0 and a <= 40:
+        print("Grade: F")
+else:
+    print("Invalid marks. Please enter marks between 0 and 100.")
+
