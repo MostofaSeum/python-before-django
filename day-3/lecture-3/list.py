b2fc794303872f1f1c0eebc1a25474fa38ff64ef
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-21 10:07:29 +0600

    feat: add lecture files covering basic list operations and methods

--- day-3/lecture-3/list.py
+++ day-3/lecture-3/list.py
@@ -1,4 +1,8 @@
+#Video link: https://youtu.be/qVyvmzFxF_o?si=_I4oJIXhRkWm_kCq
+
 marks = [85, 92, 78, 95, 88]
 print(marks)
 print(len(marks))
-print(marks[1:3])
+print(marks[2])
+print(marks[1:4])
+print(type(marks))
