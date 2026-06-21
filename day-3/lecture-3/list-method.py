e9761aa4313c1610a25774919ef60d10675f4cb8
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-21 10:53:09 +0600

    feat: add list method examples demonstrating append, sort, and insert operations

---
+++ day-3/lecture-3/list-method.py
@@ -0,0 +1,13 @@
+# list=[2,5,7]
+# list.append(5)
+# print(list)
+# list.sort()
+# print(list)
+
+# list=['a','b','c','d','e']
+# list.sort(reverse=True)
+# print(list)
+
+list = [11,6,8,]
+list.insert(1,4)
+print(list)
