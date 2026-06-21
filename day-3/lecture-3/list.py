35eced9c3fd0b777a817cf839b7ffa4b7eaddf90
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-21 11:23:17 +0600

    feat: add list operations and palindrome check script

--- day-3/lecture-3/list.py
+++ day-3/lecture-3/list.py
@@ -7,4 +7,20 @@
 # print(marks[1:4])
 # print(type(marks))
 
+#practice 1
+# a = input("Enter Your First Favourite Movie: ")
+# b = input("Enter Your Second Favourite Movie: ")
+# c = input("Enter Your Third Favourite Movie: ")
 
+# movies = [a,b,c]
+# print(movies)
+# print(type(movies))
+
+#practice 2 palindrome check
+list = [1,2,3,2,1]
+a =  list.copy()
+a.reverse()
+if list == a:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
