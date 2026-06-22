085e6320a01193486ab4d43fb4d3a367770ec757
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-22 17:02:21 +0600

    feat: add recursion examples including factorial and summation functions

--- day-04/lecture-6/recursion.py
+++ day-04/lecture-6/recursion.py
@@ -17,3 +17,19 @@ def fact(num):
         return num * fact(num-1)
 
 #print(fact(5))
+
+#Practice Question 1
+#num = int(input("Enter a number: "))
+def sum(num):
+    if num == 0:
+        return 0
+    return num + sum(num-1)
+
+# print(sum(num))
+
+#Practice Question 2
+collections = ["apple", "banana","cherry"]
+def print_collection(list):
+    print(print_collection(list[(list.length-1)]))
+
+print_collection(collections)
