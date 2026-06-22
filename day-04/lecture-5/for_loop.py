05bd0830278ed04f0807fa9933a228b1f4dc5edc
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-22 12:26:50 +0600

    feat: add for_loop.py with examples for list iteration and searching

--- day-04/lecture-5/for_loop.py
+++ day-04/lecture-5/for_loop.py
@@ -1,5 +1,21 @@
 #video link: https://youtu.be/S73thl0AyFU?si=4MD0SGMhpLfXWnRe
 
-list = [1,2,3]
-for i in list:
-    print(i)
+#printing a list elements
+# list = [1,4,9,16,25,36,49,64,81,100]
+# for i in list:
+#     print(i)
+
+#searching an element in list
+# list = [1,4,9,16,25,36,49,64,81,100]
+# x = int(input("Enter a number to search: "))
+
+# for i in list:
+#     if i == x:
+#         print("Index of ", x , "is", list.index(x))
+#         break
+# else:
+#     print("Element is not present in the list")
+
+# for i in range(5):
+#     print(i)
+
