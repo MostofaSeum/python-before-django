78b232fcd5749f36403364c24d7f663b15923ad0
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-22 15:37:19 +0600

    feat: add demonstration of function definitions and return statements in functions.py

---
+++ day-04/lecture-6/functions.py
@@ -0,0 +1,18 @@
+# video link: https://youtu.be/OvTH-7ESoRA?si=zcz9KMRrMtHDgLti
+
+def cal_sum(a,b):
+    sum = a+b
+    print(sum)
+# cal_sum(10,20)
+
+def cal_sum(a,b):
+    return a+b
+# print(cal_sum(10,20))
+
+#average of 3 numbers
+# def avg(a,b,c):
+#     print((a+b+c)/3)
+
+# avg(10,20,30)
+
+
