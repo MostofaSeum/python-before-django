5f7fda6cc4329be3754a97377de7bc2fa1bcf6b7
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-23 11:05:07 +0600

    feat: implement file read and write operations with demonstration text file

--- day-05/lecture-7/Input-Output.py
+++ day-05/lecture-7/Input-Output.py
@@ -1,3 +1,17 @@
-f = open("demo.txt", "r") 
-print(f.read()) 
+#video link: https://youtu.be/jU0cndZziO0?si=wfpDzbgUoAK1s0dS
+
+# File Reading
+# f = open("demo.txt", "r") 
+# # print(f.read()) 
+# line1 = f.readline()
+# print(line1,end="")
+
+# line2 = f.readline()
+# print(line2,end="")
+# f.close()
+
+#File Writing
+f = open("demo.txt", "a")
+f.write("\nJust trying to add another appended line")
+
 f.close()
