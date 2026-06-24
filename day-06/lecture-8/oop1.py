8ab9e305c91ecb1e460e76dc47a85cd0602eeff6
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-24 10:44:19 +0600

    feat: add initial implementation of Student class with constructor, attributes, and methods

--- day-06/lecture-8/oop1.py
+++ day-06/lecture-8/oop1.py
@@ -7,9 +7,27 @@
 # s1 = Student()
 # print(s1.name)
 
+
+
+# class Student:
+#     college_name = "ABC College" #class attribute
+#     def __init__(self,fullname):#instance attribute
+#         print("This is constructor")
+#         self.name = fullname
+# s1 = Student("Seum")
+# print(s1.college_name)
+# print(s1.name)
+
+
+
 class Student:
-    def __init__(self,fullname):
+    college_name = "ABC College" #class attribute
+    def __init__(self,fullname):#instance attribute
         print("This is constructor")
         self.name = fullname
+    def hello(self):
+        print("Hello", self.name)
 s1 = Student("Seum")
+print(s1.college_name)
 print(s1.name)
+s1.hello()
