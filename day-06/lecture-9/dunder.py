5dca0960ce70a0bec441d3e1694ded56fa51b49d
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-24 16:39:30 +0600

    feat: implement complex number addition using dunder method __add__

---
+++ day-06/lecture-9/dunder.py
@@ -0,0 +1,17 @@
+class complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+    def show_complex(self):
+        print(self.real, "i + ",self.img,"j")
+
+    def __add__(self,other):
+        return complex(self.real + other.real, self.img + other.img)
+
+c1 = complex(1,2)
+c2 = complex(3,4)
+c1.show_complex()
+c2.show_complex()
+c3 = c1+c2
+c3.show_complex()
+    
